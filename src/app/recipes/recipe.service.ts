2069b87375b2c98248e3a993cd3aaad775ac7530
RichLira <ricardodelavega@gmail.com>
2016-10-19 20:25:45 -0500

    Agregado comidas con titulo, descripcion e imagenes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,8 +10,10 @@ export class RecipeService {
 			new Ingredient('Carne de Suadero', 1),
 			new Ingredient('Tortilla', 1)
 			]),
-		new Recipe('Flautas ahogadas en salsa verde', 'Flautas rellenas de pechuga de pollo deshebrada, bañadas en una salsa de tomate verde y servidas con crema y queso.', 'http://www.richlira.com/comidas/comida1.jpg', []),
+		new Recipe('Chilaquiles verdes', 'Los Chilaquiles Verdes son un plato típico Mexicano, el cual consiste en las tortillas que utilizamos para los típicos tacos en casa.', 'http://www.richlira.com/comidas/comida1.jpg', []),
 		new Recipe('Enchiladas verdes', 'Enchiladas en Salsa Verde. Un delicioso desayuno o platillo principal, tortillas de maíz rellenas de pollo y bañadas en una salsa verde, crema y queso.', 'http://www.richlira.com/comidas/comida2.jpg', []),
+		new Recipe('Enfrijoladas de pollo con queso', 'Receta de enfrijoladas de pollo con queso, un plato típico de la cocina mexicana: rollos de tortillas de maíz rellenas con pollo, salsa de frijoles y queso.', 'http://www.richlira.com/comidas/comida3.jpg', []),
+		new Recipe('Nachos con guacamole', 'En una comida o cena mexicana no debe faltar este aperitivo tradicional de nachos acompañados de guacamole, una salsa elaborada con aguacate.', 'http://www.richlira.com/comidas/comida4.jpg', [])
 ];
 
   constructor() { }
